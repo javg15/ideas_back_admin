02f6011dc0c90b380cbaa367e8762d7b03a8f34e
Acortez08530 <acortez.08530@cobaev.edu.mx>
2023-02-20 11:26:07 -0600

    update

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,6 +1,7 @@
 const ctrl = {};
 ctrl.auth = require("../controllers/auth.controller.js");
 ctrl.user = require("../controllers/user.controller.js");
+ctrl.catpreguntas = require("../controllers/catpreguntas.controller.js");
 
 exports.index = (req, res) => {
     /*{
@@ -20,6 +21,7 @@ exports.index = (req, res) => {
         case "signup": ctrl.auth.signup(req, res); break;
         case "signin": ctrl.auth.signin(req, res); break;
         case "getmenu": ctrl.user.getMenu(req, res); break;
+        case "/catpreguntas/getcuestionario":ctrl.catpreguntas.getCuestionario(req, res); break;
         default:res.status(200).send(
             { 
                 codigo:"00100",
